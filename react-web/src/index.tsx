8ea6634488c87c3a676aae1fcdcc231c88d12ffc
Jim Karg <wurmr@users.noreply.github.com>
2020-06-17 15:06:19 +0000

    add client side proxy

--- react-web/src/index.tsx
+++ react-web/src/index.tsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import ApolloClient, { gql } from 'apollo-boost'
+import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from '@apollo/react-hooks'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: '/api',
 })
 
 ReactDOM.render(
@@ -22,4 +22,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.register()
