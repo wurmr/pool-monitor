79e57cff63a8b772d23de740580bac8a0efdf291
Jim Karg <wurmr@users.noreply.github.com>
2020-06-24 10:32:12 -0500

    abstract ADC interface and add memoization

--- monitor/src/resolvers/index.ts
+++ monitor/src/resolvers/index.ts
@@ -2,24 +2,18 @@ import { IResolvers } from 'apollo-server'
 import { promises as fs } from 'fs'
 import { getTemperature } from '../sensors/temperature'
 import { getPressure } from '../sensors/pressure'
+import { readInput } from '../seed/adc'
 
 const resolvers: IResolvers = {
   Query: {
     state: () => ({
       temperature: async () => {
-        const reading = await fs.readFile(
-          '/sys/devices/platform/soc/fe804000.i2c/i2c-1/1-0048/in4_input',
-          'utf-8'
-        )
-        return getTemperature(parseInt(reading))
+        const reading = await readInput(4)
+        return getTemperature(reading)
       },
       pressure: async () => {
-        const reading = await fs.readFile(
-          '/sys/devices/platform/soc/fe804000.i2c/i2c-1/1-0048/in7_input',
-          'utf-8'
-        )
-
-        return getPressure(parseInt(reading))
+        const reading = await readInput(7)
+        return getPressure(reading)
       },
     }),
   },
---
+++ monitor/src/seed/adc.ts
@@ -0,0 +1,15 @@
+import { promises as fs } from 'fs'
+import memoize from 'memoizee'
+
+type inputRange = 4 | 7
+
+const readInputRaw = async (input: inputRange) => {
+  const reading = await fs.readFile(
+    `/sys/devices/platform/soc/fe804000.i2c/i2c-1/1-0048/in${input}_input`,
+    'utf-8'
+  )
+
+  return parseInt(reading)
+}
+
+export const readInput = memoize(readInputRaw, { maxAge: 500 })
