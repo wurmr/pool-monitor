ce8349e9a44ebedb08e8c9c058860af62db3aa43
Jim Karg <wurmr@users.noreply.github.com>
2020-06-25 12:50:01 -0500

    updated pressure logic

--- monitor/src/sensors/pressure.ts
+++ monitor/src/sensors/pressure.ts
@@ -1,8 +1,16 @@
-const MAX_READING = 2048
+// Value (mv) at 0 PSI
+const MV_ZERO_READING = 180
+
+// Value (mv) at 30 PSI
+const MV_FULL_SCALE = 1860
+
+// Meter Full Scale (PSI)
+const PSI_FULL_SCALE = 30
 
 export const getPressure = (adcInput: number) => {
-  const psi = ((adcInput - 256) * 30) / MAX_READING
-
+  const psi =
+    ((adcInput - MV_ZERO_READING) * PSI_FULL_SCALE) /
+    (MV_FULL_SCALE - MV_ZERO_READING)
   return {
     psi,
   }
