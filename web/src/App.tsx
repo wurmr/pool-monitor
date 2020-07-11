5f063d70607f062f6859b6b032fa267b4bdb6e98
Jim Karg <wurmr@users.noreply.github.com>
2020-07-11 15:31:06 -0500

    add moving average to frontend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import './App.css'
 import { gql } from 'apollo-boost'
 import { useQuery } from '@apollo/react-hooks'
 import TemperatureGauge from './Components/TemperatureGuage'
 import PressureGuage from './Components/PressureGuage'
+import MA from 'moving-average'
 
 const STATE = gql`
   {
@@ -47,24 +48,48 @@ interface State {
   }
 }
 
+interface Averages {
+  temperature: number
+  pressure: number
+}
+
 function App() {
   const { loading, error, data } = useQuery<QueryData>(STATE, {
     pollInterval: 1000,
   })
 
+  const timeInterval = 20 * 1000 // 20 seconds
+  const mTemperture = useRef(MA(timeInterval))
+  const maPressure = useRef(MA(timeInterval))
+  const [state, setState] = useState<State>()
+  const [averages, setAverages] = useState<Averages>()
+
+  useEffect(() => {
+    if (!data) return
+
+    mTemperture.current.push(Date.now(), data.state.temperature.fahrenheit)
+    maPressure.current.push(Date.now(), data.state.pressure.psi)
+
+    setAverages({
+      temperature: mTemperture.current.movingAverage(),
+      pressure: maPressure.current.movingAverage(),
+    })
+    setState(data.state)
+  }, [data])
+
   if (loading) return <p>Loading...</p>
-  if (error || !data) return <p>Error :(</p>
+  if (error || !state || !data || !averages) return <p>Error :(</p>
 
   return (
     <div className="App">
       <header className="App-header">
         <TemperatureGauge
-          temperature={data.state.temperature.fahrenheit}
-          millivolts={data.state.adc.tempetureChannel.millivolts}
+          temperature={averages.temperature}
+          millivolts={state.adc.tempetureChannel.millivolts}
         />
         <PressureGuage
-          pressure={data.state.pressure.psi}
-          millivolts={data.state.adc.pressureChannel.millivolts}
+          pressure={averages.pressure}
+          millivolts={state.adc.pressureChannel.millivolts}
         />
       </header>
     </div>
---
+++ web/types/moving-average/index.d.ts
@@ -0,0 +1,3 @@
+declare function MovingAverage(timespan: any): any
+
+declare module 'moving-average'
