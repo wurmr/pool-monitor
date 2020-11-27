cc3479bbf36229cbdae25685f20fa7511d63eaf9
Jim Karg <wurmr@users.noreply.github.com>
2020-11-27 17:42:47 -0600

    Update to industrial i/o

--- monitor/src/seed/adc.ts
+++ monitor/src/seed/adc.ts
@@ -2,8 +2,8 @@ import { promises as fs } from 'fs'
 import memoize from 'memoizee'
 import { BehaviorSubject } from 'rxjs'
 
-export type ChannelNumber = 0 | 1 | 2 | 3
-type InputRange = 4 | 5 | 6 | 7
+export type ChannelNumber = 0 | 3
+type InputRange = 0 | 3
 
 type Channels =
   | {
@@ -17,12 +17,14 @@ interface Channel {
 }
 
 const readInputRaw = async (input: InputRange) => {
-  const reading = await fs.readFile(
-    `/sys/devices/platform/soc/fe804000.i2c/i2c-1/1-0048/in${input}_input`,
-    'utf-8'
-  )
+  const filename = `/sys/devices/platform/soc/fe804000.i2c/i2c-1/1-0048/iio\:device0/in_voltage${input}_raw`
+  const scaleFilename = `/sys/devices/platform/soc/fe804000.i2c/i2c-1/1-0048/iio\:device0/in_voltage${input}_scale`
 
-  return parseInt(reading)
+  const scaleReading = await fs.readFile(scaleFilename, 'utf-8')
+  const reading = await fs.readFile(filename, 'utf-8')
+
+  const scaledValue = parseInt(reading) * parseFloat(scaleReading)
+  return scaledValue
 }
 
 const readInput = memoize(readInputRaw, { maxAge: 1000, preFetch: true })
@@ -31,29 +33,16 @@ export const adcChannels = new BehaviorSubject<Channels>(undefined)
 
 export const startPolling = (ms: number) => {
   const interval = setInterval(async () => {
-    const rawValues = await Promise.all([
-      readInput(4),
-      readInput(5),
-      readInput(6),
-      readInput(7),
-    ])
+    const rawValues = await Promise.all([readInput(0), readInput(3)])
 
     const channels = {
       0: {
-        id: 4,
+        id: 0,
         value: rawValues[0],
       },
-      1: {
-        id: 5,
-        value: rawValues[1],
-      },
-      2: {
-        id: 6,
-        value: rawValues[2],
-      },
       3: {
-        id: 7,
-        value: rawValues[3],
+        id: 3,
+        value: rawValues[1],
       },
     } as const
 
